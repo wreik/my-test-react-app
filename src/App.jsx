6b42485b918420563aecb700b145b6b00d5462a9
William Reik <wreik@hotmail.com>
2026-02-20 15:56:44 -0500

    added environment variable

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>{import.meta.env.VITE_TEST_VAR}</p>
     </>
   )
 }
